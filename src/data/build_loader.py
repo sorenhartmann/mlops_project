0607d6c03de3b9d9556b06449ba7315a2a4002de
Nicolai Piet Dittmann <s170589@student.dtu.dk>
2021-06-10 14:36:43 +0200

    backbone setup

--- src/data/build_loader.py
+++ src/data/build_loader.py
@@ -3,6 +3,16 @@ from transformers import ConvBertTokenizer
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data.dataset import random_split
 
+class terminal_colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def build_train_val_loader(df, batch_size, device, val_size):
 
@@ -16,7 +26,7 @@ def build_train_val_loader(df, batch_size, device, val_size):
         torch.tensor(x.input_ids).to(device),
         torch.tensor(x.token_type_ids).to(device),
         torch.tensor(x.attention_mask).to(device),
-        torch.tensor(df.target).float().to(device))
+        torch.tensor(df.target, dtype = torch.long).to(device))
 
     # split
     val_ds_size = int(len(ds) * val_size)

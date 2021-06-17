fd741f93795b3ec4ee3de53ee4976e94c4030478
Nicolai Piet Dittmann <s170589@student.dtu.dk>
2021-06-17 14:01:00 +0200

    added profiling option

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -10,18 +10,28 @@ from pytorch_lightning.loggers import WandbLogger
 from src.data.datamodule import DisasterDataModule
 from src.models.model import ConvBert
 import wandb
+from pytorch_lightning.profiler import AdvancedProfiler,PyTorchProfiler
 def main(args):
 
     wandb_api = wandb.Api()
 
     wandb_logger = WandbLogger(project="mlops_project", entity="mlops_project")
     
-    dm = DisasterDataModule(**vars(args))
+    dm = DisasterDataModule("./data", batch_size=16)
 
     model = ConvBert(**vars(args))
+
+    if args.Aprofiler:
+        #profiler = AdvancedProfiler(dirpath = 'reports', filename = 'profiler')
+        profiler = PyTorchProfiler(dirpath = 'reports', filename = 'profiler.prof', sort_by_key = 'cuda_time')
+    else:
+        profiler = None
+
     trainer = pl.Trainer.from_argparse_args(
         args,
+        profiler = profiler,
         logger=wandb_logger,
+        accelerator ='ddp'
     )
 
     trainer.fit(model, dm)
@@ -31,9 +41,10 @@ if __name__ == "__main__":
     parser = ArgumentParser()
 
     parser = ConvBert.add_model_specific_args(parser)
-    parser = DisasterDataModule.add_data_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
+    parser.add_argument('--Aprofiler', action='store_true')
 
     args = parser.parse_args()
+    
 
     main(args)

9abc327ea2c8a9634ac4abb173a350846d3f15a3
unknown <s174344@student.dtu.dk>
2021-06-17 14:50:11 +0200

    torch serve script

---
+++ torch_serve/model_script.py
@@ -0,0 +1,33 @@
+import torch
+from src.models.model import ConvBert
+import os, sys, argparse
+import os
+
+parser = argparse.ArgumentParser(description='Model deploying arguments')
+parser.add_argument('--model_name', default='convbert_model')
+parser.add_argument('--version', default=1.0)
+parser.add_argument('--serialize_model', action='store_true')
+parser.add_argument('--deploy_model', action='store_true')
+parser.add_argument('--model_path', default='models/checkpoint.pth')
+args = parser.parse_args(sys.argv[1:2])
+
+# model = ConvBert(lr=0.01, **{})
+
+# # #TODO insert load of state dict
+
+# script_model = torch.jit.script(model)
+# script_model.save('torch_serve/' + str(args.model_name) + '.pt')
+
+if args.serialize_model:
+    ss = 'torch-model-archiver --model-name ' + str(
+        args.model_name) + ' --version ' + str(
+            args.version) + ' --serialized-file '
+    ss = ss + 'torch_serve/' + str(
+        args.model_name
+    ) + '.pt' + ' --export-path model_store --extra-files torch_serve/index_to_name.json --handler text_classifier'
+    os.system(ss)
+
+if args.deploy_model:
+    ss = 'torchserve --start --ncs --model-store model_store --models ' + str(
+        args.model_name) + '=' + str(args.model_name) + '.mar'
+    os.system(ss)
